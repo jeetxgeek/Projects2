291974f39ee93a3f6d9940dffb18fcd7caf9c949
jeetxgeek <mauryajeetesh57@gmail.com>
2024-05-22 18:16:34 +0530

    fibonnacci series updated user defined added!

--- Fibonacciseries.java
+++ Fibonacciseries.java
@@ -1,13 +1,20 @@
 package Projects2;
 
+import java.util.Scanner;
+
 /**
  * Fibonacciseries
  */
 public class Fibonacciseries {
     public static void main(String args[])
-    {  
-     int n1=0,n2=1,n3,i,count=10;  
-     System.out.print(n1+" "+n2);//printing 0 and 1  
+    { 
+        Scanner f = new Scanner(System.in);
+        
+        int n1=0,n2=1,n3,i;  
+        System.out.println("enter the no. of series:");
+        int count = f.nextInt();
+
+        System.out.print(n1+" "+n2);//printing 0 and 1  
       
      for(i=2;i<count;++i)//loop starts from 2 because 0 and 1 are already printed  
      {  
